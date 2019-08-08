df094e4ccdceca0446624048871cc809b752a326
rudolif <rudolifrudolif@gmail.com>
2019-08-08 18:12:03 +0200

    start commit

---
+++ app/js/common.js
@@ -0,0 +1,37 @@
+$(function() {
+
+//------------------------------гамбургер-----------------------------
+  $('.hamburger').click(function() {
+    $(this).toggleClass('hamburger--active');
+    $('nav').toggleClass('nav--active');
+    $('header').toggleClass('header--menu');
+  });
+
+//-------------------------------попандер---------------------------------------
+  $('.modal').popup({transition: 'all 0.3s'});
+
+//----------------------------------------fixed----------------------------------
+  $(window).scroll(function(){
+      if($(this).scrollTop()>20){
+          $('.header').addClass('header--active');
+      }
+      else if ($(this).scrollTop()<20){
+          $('.header').removeClass('header--active');
+      }
+  });
+
+//-------------------------скорость якоря---------------------------------------
+  $(".header__list").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top - 60}, 'slow', 'swing');
+  //--------------------закриття меню при кліку на ссилку якоря--------------------
+     // $('.hamburger').removeClass('hamburger--active');
+     // $('.header-menu').removeClass('header-menu');
+     // $('.header--active').removeClass('header--active');
+     // $('.nav--active').removeClass('nav--active');
+
+  });
+  
+});
