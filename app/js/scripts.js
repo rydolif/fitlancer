00c40c467e5f9594bd1931b87fdba6c412cf4691
rudolif <rudolifrudolif@gmail.com>
2019-08-09 11:40:10 +0200

    index page commit

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -856,35 +856,35 @@ $(function() {
 //------------------------------гамбургер-----------------------------
   $('.hamburger').click(function() {
     $(this).toggleClass('hamburger--active');
-    $('nav').toggleClass('nav--active');
-    $('header').toggleClass('header--menu');
+    $('.nav').toggleClass('nav--active');
+    $('body').toggleClass('body--menu');
   });
 
 //-------------------------------попандер---------------------------------------
   $('.modal').popup({transition: 'all 0.3s'});
 
-//----------------------------------------fixed----------------------------------
-  $(window).scroll(function(){
-      if($(this).scrollTop()>20){
-          $('.header').addClass('header--active');
-      }
-      else if ($(this).scrollTop()<20){
-          $('.header').removeClass('header--active');
-      }
-  });
+// //----------------------------------------fixed----------------------------------
+//   $(window).scroll(function(){
+//       if($(this).scrollTop()>20){
+//           $('.header').addClass('header--active');
+//       }
+//       else if ($(this).scrollTop()<20){
+//           $('.header').removeClass('header--active');
+//       }
+//   });
 
-//-------------------------скорость якоря---------------------------------------
-  $(".header__list").on("click","a", function (event) {
-      event.preventDefault();
-      var id  = $(this).attr('href'),
-          top = $(id).offset().top;
-      $('body,html').animate({scrollTop: top - 60}, 'slow', 'swing');
-  //--------------------закриття меню при кліку на ссилку якоря--------------------
-     // $('.hamburger').removeClass('hamburger--active');
-     // $('.header-menu').removeClass('header-menu');
-     // $('.header--active').removeClass('header--active');
-     // $('.nav--active').removeClass('nav--active');
+// //-------------------------скорость якоря---------------------------------------
+//   $(".header__list").on("click","a", function (event) {
+//       event.preventDefault();
+//       var id  = $(this).attr('href'),
+//           top = $(id).offset().top;
+//       $('body,html').animate({scrollTop: top - 60}, 'slow', 'swing');
+//   //--------------------закриття меню при кліку на ссилку якоря--------------------
+//      // $('.hamburger').removeClass('hamburger--active');
+//      // $('.header-menu').removeClass('header-menu');
+//      // $('.header--active').removeClass('header--active');
+//      // $('.nav--active').removeClass('nav--active');
 
-  });
+//   });
   
 });
