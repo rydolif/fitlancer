692722efcf5b578fa975594fe05cc17b6e28120a
rudolif <rudolifrudolif@gmail.com>
2019-08-13 20:12:39 +0200

    profile-massang page commit

--- gulpfile.js
+++ gulpfile.js
@@ -54,12 +54,8 @@ gulp.task('minjs', function() {
 		// 'app/libs/jquery.validate.js', 					//----форма
 		// 'app/libs/jquery.mask.min.js', 					//----форма
 		'app/libs/jquery.popupoverlay.js', 				//----модалки
-		// 'app/libs/slick/slick.js', 					//----слайдер
-		 // 'app/libs/flipclock/flipclock.js',
-		// 'app/libs/swiper/swiper.min.js', 			//----слайдер
-		// 'app/libs/fancybox/jquery.fancybox.js', 		//----картінка прикліку
-		// 'app/libs/jquery.spincrement.min.js', 		//----цифри анімованні
-		// 'app/libs/isotope.pkgd.min.js', 				//----сетка елементов + фильтр
+		'app/libs/jscrollpane/jquery.jscrollpane.js', 				//----модалки
+		'app/libs/jscrollpane/jquery.mousewheel.js', 				//----модалки
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(plumber())
